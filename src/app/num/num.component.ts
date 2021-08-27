64d2fbcc7a8ebac6c8484b66d47ee148e83fbfc2
isabeela <isabeelabg@gmail.com>
2021-08-26 23:08:05 -0300

    1 file modified, 1 added

--- src/app/num/num.component.ts
+++ src/app/num/num.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./num.component.css']
 })
 export class NumComponent implements OnInit {
+  aleatorio = Math.floor(Math.random() * 101);
+  aleatorio1 = Math.floor(Math.random() * 101);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getAleatorio() {
+    let list: number[] = [];
+    for (let i = 1; i <= 5; i++) {
+      list.push(this.aleatorio, this.aleatorio1);
+    }
+    return list;
   }
-
 }
